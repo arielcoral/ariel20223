1654fe8ce88d10f276819c06c141ef369e588d57
arielcoral <123407136+arielcoral@users.noreply.github.com>
2023-02-20 12:14:47 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,111 @@
+// משימה ראשונה
+// const names = [
+//     "Avior",
+//     "Avital",
+//     "Noam",
+//     "Sigal",
+//     "Samdar",
+// ];
+
+// for (let index = 0; index < names.length; index++) {
+//     const name = names[index];
+//     console.log(name)
+// }
+// משימה ראשונה
+// משימה שניה
+//  for (var i = 1; i <= 10; i++){
+//     var line = ""
+//     for(var j = 1; j<= 10; j++){
+//         product = (i*j == 72 || i*j == 50 || i*j == 100) ?
+//         "*"+(i*j)+"*":
+//         (i*j).toString().padEnd(4, ' ')
+//         line += product
+//     }
+//     console.log(line)
+//  }    
+
+// משימה שניה
+// משימה שלישית 
+// var answer = prompt('Please type a number')
+// console.log(answer)
+// משימה שלישית
+// משימה רביעית
+// var sto = "STOP"
+// var sto = prompt("please enter");
+// var index = 0;
+// while (sto !== "STOP") {
+//     console.log(index)
+//     index++
+// }
+// משימה רביעית
+// משימה חמישית 
+
+// var word;
+// while (isNaN(word)){
+//         var word = prompt('please enter a number');
+// }
+
+// var i = 0;
+//         while(i <= word){
+//                  console.log(i)
+//                  i++
+//          }
+ 
+// משימה חמישית 
+// משימה שישית 
+
+// const upils = [
+//     "Avior",
+//     "Avital",
+//     "Noam",
+//     "Sigal",
+//     "Samdar"
+// ];
+
+// var names = prompt("please enter a name");
+// var word = false
+// for (i in upils){
+//         if (upils[i] === names){
+//                 word = true;
+//         }
+// }
+// console.log(word)
+
+// משימה שישית
+// משימה שביעית
+
+// var display = "*";
+// display = display + "*" 
+// var index = 0; 
+// while (index < 3){ 
+// 	display = display + "*" 
+// 	index++
+// }
+// var j = 0;
+// while(j < 5){
+// console.log(display)
+// j++
+// }
+
+
+// משימה שביעית
+// משימה שמינית
+
+// var word = prompt('lease enter a number');
+// var display = '';
+// display = display + '*'
+// for(var i = 1; i < word; i++){
+// 	display = display + '*'
+// }
+// for (var j = 0; j < word; j++){
+// 	console.log(display)
+// }
+// משימה שמינית
+// משימה תשיעית
+
+// var asterisk = '*';
+// for (var i = 1; i < 8; i++){
+//         console.log(asterisk)
+//         asterisk = i < 4 ? asterisk+='*' : asterisk.substring(0, asterisk.length-1);
+// }
+// משימה תשיעית
